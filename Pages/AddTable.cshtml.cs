3e2d60ff50e60c76442b83185727e0a101239102
Ongarj Thongchai <ongarj2314@hotmail.com>
2023-10-28 16:18:49 +0700

    Edit Error Show With BlazorBootstrap And Delete File Ex.

---
+++ Pages/AddTable.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeachingSchedule.Pages
+{
+    public class AddTableModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
